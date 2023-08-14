b771d209eeaf242d3a13ef1e3ca5cf4134f87aa7
AntoXa PRO <info@antoxa.pro>
2023-08-14 16:45:26 +0300

    ToggleTheme

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import UiIcon from './Icon.vue'
+
+// Props.
+const props = defineProps<{
+	dark?: boolean
+}>()
+
+// Emits.
+const emit = defineEmits<{ (e: 'update:dark', v: boolean): void }>()
+</script>
+
+<template>
+	<div class="ui-toggle-theme">
+		<ui-icon
+			:name="props.dark ? 'moon' : 'sun'"
+			@click="emit('update:dark', !props.dark)"
+			title="Переключить тему"
+		/>
+	</div>
+</template>
